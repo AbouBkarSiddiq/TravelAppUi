73ce4264a6e319cca11183c8dbbfc16005937a1f
Abou Bkar Siddiq <aboubkar786@gmail.com>
2021-04-19 01:25:14 +0500

    Navigation Part done

---
+++ components/Liked.js
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Liked = () => {
+  return (
+    <View>
+      <Text>Liked</Text>
+    </View>
+  );
+};
+
+export default Liked;
